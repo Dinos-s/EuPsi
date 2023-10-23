90340a18d7625643162c72979eb90f5104d25b8e
Gabriel <gabriel02ramos@gmail.com>
2023-10-23 18:49:37 -0300

    criand o form de cadastro & validator

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 // Validação do formulario de cadastro psicolgo
 const form = document.getElementById('form');
+const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const senhaRepeat = document.getElementById('senha-repeat');
 const submitBtn = document.getElementById('submitBtn');
@@ -14,7 +15,7 @@ function validarForm() {
 
     // email é valido
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailPattern.test(email)) {
+    if (!emailPattern.test(email.value)) {
         alert('Por favor, insira um email válido.');
         return false;
     }
