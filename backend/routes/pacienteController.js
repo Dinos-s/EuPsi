01ba2ed3468ade70852d6e986ce23e62f44a427c
Gabriel <gabriel02ramos@gmail.com>
2023-11-22 19:53:20 -0300

    Criando a rota de login para autenticaçao do paciente

--- backend/routes/pacienteController.js
+++ backend/routes/pacienteController.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 let router = express.Router();
 import pacienteService from '../services/PacienteSevice.js'
 
@@ -7,9 +9,9 @@ router.post('/addPaciente', async (req, res) => {
     const { nome, telefone, email, senha } = req.body
 
     //emcriptando a senha
-    const salt = await bcrypt.genSalt(15)
+    const salt = await bcrypt.genSalt(12)
     const senhaHash = await bcrypt.hash(senha, salt)
-    
+
     const PacienteModel = {
         nome: nome,
         telefone: telefone,
@@ -20,6 +22,37 @@ router.post('/addPaciente', async (req, res) => {
     const paciente = await pacienteService.savePaciente(PacienteModel)
     return res.status(201).json(paciente)
 })
+
+//rota de login do paciente
+router.post('/paciente/login', async (req, res) => {
+    const { email, senha } = req.body
+
+    try {
+        // Verificar se o paciente existe no banco de dados
+        const paciente = await pacienteService.getPacienteByEmail(email);
+
+        if (!paciente) {
+            return res.status(401).json({ mensagem: 'Credenciais inválidas email' });
+        }
+
+        // Verificar se a senha fornecida corresponde à senha armazenada no banco de dados
+        const senhaCorreta = await bcrypt.compare(senha, paciente.senha);
+
+        if (!senhaCorreta) {
+            return res.status(401).json({ mensagem: 'Credenciais inválidas senha'});
+        }
+
+        // Gerar token de autenticação
+        const token = jwt.sign({ id: paciente.id }, 'seuSegredoDoToken', {
+            expiresIn: '1h',
+        });
+        
+        return res.status(200).json({ token });
+    } catch (error) {
+        console.error('Erro durante o login:', error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+    }
+});
 
 router.get('/pacientes', async (req, res) => {
     const allPacientes = await pacienteService.getAllPacientes()
@@ -33,7 +66,7 @@ router.get('/paciente/:id', async (req, res) => {
 
 router.put('/updatePaciente/:id', async (req, res) => {
     const { nome, telefone, email, senha } = req.body
-    
+
     const PacienteModel = {
         nome: nome,
         telefone: telefone,
